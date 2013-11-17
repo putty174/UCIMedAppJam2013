9fdc97dea6704ffff7e2bd5a7ecad4b1b2527b64
putty174 <myh10232223@yahoo.com>
2013-11-17 15:51:46 -0800

    Symptom Viewer

---
+++ TopSecretProjectForTeam11/SymptomViewController.h
@@ -0,0 +1,13 @@
+//
+//  SymptomViewController.h
+//  TopSecretProjectForTeam11
+//
+//  Created by Max on 11/17/13.
+//  Copyright (c) 2013 App Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SymptomViewController : UIViewController
+
+@end
---
+++ TopSecretProjectForTeam11/SymptomViewController.m
@@ -0,0 +1,47 @@
+//
+//  SymptomViewController.m
+//  TopSecretProjectForTeam11
+//
+//  Created by Max on 11/17/13.
+//  Copyright (c) 2013 App Jam. All rights reserved.
+//
+
+#import "SymptomViewController.h"
+
+@interface SymptomViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *ContentView;
+
+@end
+
+@implementation SymptomViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    [self.scrollView layoutIfNeeded];
+    self.scrollView.contentSize = self.ContentView.bounds.size;
+}
+
+@end
