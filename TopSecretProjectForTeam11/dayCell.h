8b1bd7957b89ff9a32ed61de98ffa29586168d49
pzvla <paz33.1b@gmail.com>
2013-11-16 16:19:54 -0800

    calendar start

---
+++ TopSecretProjectForTeam11/dayCell.h
@@ -0,0 +1,14 @@
+//
+//  dayCell.h
+//  TopSecretProjectForTeam11
+//
+//  Created by Mary Nguyen on 11/16/13.
+//  Copyright (c) 2013 App Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dayCell : UICollectionViewCell
+@property (strong, nonatomic) IBOutlet UILabel *number;
+
+@end
