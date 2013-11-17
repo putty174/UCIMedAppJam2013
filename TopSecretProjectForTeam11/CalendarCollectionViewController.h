c9276d8597af8e1c3e8d11b1eae0e8acbf80bc26
putty174 <myh10232223@yahoo.com>
2013-11-16 19:48:04 -0800

    Revert 2417f96..8608d68
    This rolls back to commit 2417f96c29095440f90ac2e1ae8e34a2022162e1.

---
+++ TopSecretProjectForTeam11/CalendarCollectionViewController.h
@@ -0,0 +1,14 @@
+//
+//  CalendarCollectionViewController.h
+//  TopSecretProjectForTeam11
+//
+//  Created by Mary Nguyen on 11/16/13.
+//  Copyright (c) 2013 App Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarCollectionViewController : UICollectionViewController
+@property (strong, nonatomic) IBOutlet UICollectionViewCell *header;
+
+@end
